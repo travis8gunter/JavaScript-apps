57d4ef369e565dc2c0f08e019b2cf51681cbb9c8
Travis Gunter <53490233+travis8gunter@users.noreply.github.com>
2022-06-20 22:20:24 -0600

    added junk function
    added junk function and details.

--- daily-calories/index.js
+++ daily-calories/index.js
@@ -1,15 +1,27 @@
-console.log('Welcome to this Calorie Tracker!!');
+console.log('-----------Welcome to this Calorie Tracker!!----------');
 const name = ('Travis');
 console.log(`Hello ${name}`);
 
+/* -----------Var's for calories/junkfood and working out----------- */
+const junk = (8);
 let calorie = (1600)
 
 function calculate(calorie){ 
     if (calorie >= 1500){
-    console.log('You smashed your goal!');
+        return console.log('You smashed your goal!');
     } else {
-        console.log('You did not get your Calorie Goal!');
+        return console.log('You did not get your Calorie Goal!');
     };
 };
 
+function fat(junk){
+    if (junk >= 6){
+        return console.log('You had to much junk food!!');
+    } else {
+        return console.log('You could have another treat!!');
+    };
+};
+calculate();
+fat();
 console.log(calculate(calorie));
+console.log(fat(junk));
